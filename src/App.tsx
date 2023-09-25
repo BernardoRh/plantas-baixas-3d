bf814df1760d59ebb8f773c2e0fd197d43229318
Bernardo R. Horbach <bernardo.horbach@gmail.com>
2023-09-25 17:53:56 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import { Scene01 } from "./polygonjs/scenes/Scene01";
 
-function App() {
+export function App() {
 	return (
 		<div className="App">
 			<Scene01></Scene01>
 		</div>
 	)
 }
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { App } from "./App";
 import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("app") as HTMLElement).render(
